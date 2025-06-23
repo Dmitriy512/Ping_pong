904ca5056ba913383abe3bac6ff2eda5f5348696
Dmitriy512 <dimap8680@gmail.com>
2025-06-23 09:46:30 +0300

    Update ping_pong.py

--- ping_pong.py
+++ ping_pong.py
@@ -39,9 +39,16 @@ class Player(GameSprite):
         if keys[K_UP] and self.rect.y < 495:
             self.rect.y -= self.speed
 
-class Enemy(GameSprite):
-    def update(self):
-        
+racket1 = Player('', 20, 250, 2, 15, 100)
+racket2 = Player('', 580, 250, 2, 15, 100)
+ball = GameSprite('', 300, 250, 2, 50, 50)
+
+font1 = font.Font(None, 35)
+lose1 = font1.render('Player 1 lose!', True, (180, 0, 0))
+lose2 = font1.render('Player 2 lose!', True, (180, 0, 0))
+
+speed_x = 3
+speed_y = 3
 
 while game:
     for e in event.get():
@@ -49,6 +56,24 @@ while game:
             game = False
     if finish == True:
         game = False
+
+    if finish != True:
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+    
+    if ball.rect.y > win_wigth - 50 or ball.rect.x < 0:
+        speed_y *= -1
+
+    if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+        speed_x *= -1
+    
+    if ball.rect.x < 0:
+        finish = True
+        window.blit(lose1, (200, 200))
+        
+    if ball.rect.x > 600:
+        finish = True
+        window.blit(lose2, (200, 200))
 
     display.update()
     clock.tick(fps)
